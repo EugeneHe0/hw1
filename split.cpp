1aaf013a387790947f787905028010ace8efa9f4
Youjin Heo <youjinhe@usc.edu>
2026-09-08 17:06:49 -0700

    Implement recursive split

--- split.cpp
+++ split.cpp
@@ -10,14 +10,41 @@ split.h.  **Do NOT add main() to this file**.  When you submit
 the function below should be the only one in this file.
 */
 
+/*
+random-oneline-list of nums : divide it into (odd)list and (even)list
+after code : nums fill odd and even and the origin in is empty
+*/
+
 #include "split.h"
 
 /* Add a prototype for a helper function here if you need */
+//current->value : int / current->next : Node*
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if(in == nullptr) //when recursive hits (null)
+  {
+    return; //back to previous recursion
+  }
+
+  Node* current = in; //current also points first node of in
+  in = in->next; //current still remember first node
+                //in's point move 1,2,3,...,null(final)
+
+  split(in, odds, evens); //recursive!
+  //(1,2,3) > (2,3) > (3) > (null)
+
+  if(current->value % 2 != 0) //if the num is odd
+  {
+    current->next = odds; //current's pointer points odd's start
+    odds = current; //now odd is pointing new-added odd num list
+  }
+  else { //if the num is even
+    current->next = evens;
+    evens = current;
+  }
 }
 
 /* If you needed a helper function, write it here */
